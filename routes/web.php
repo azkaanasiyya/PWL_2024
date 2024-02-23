e95b48d4b2efbb67a7d1cbcbdd99df4ddf3f8a9b
Azka Anasiyya Haris <azkaanasiyya@gmail.com>
2024-02-23 11:16:06 +0700

    modifikasi routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,47 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//basic routing
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function() {
+    return 'World';
+});
+
+Route::get('/', function() {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function() {
+    return 'NIM: 2241720157 <br> Nama: Azka Anasiyya Haris';
+});
+
+//route parameters
+
+Route::get('/user/{Azka}', function($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post1}/comments/{comment5}', function
+($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-1: ".$commentId;
+});
+
+Route::get('/articels/{article1234}', function($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+//optional parameters
+
+Route::get('/user/{Azka?}', function($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function($name='John') {
+    return 'Nama saya '.$name;
+});
+
